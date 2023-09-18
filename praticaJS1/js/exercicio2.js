5fcd6e259c225bdce1ae0f43100ec4b812bdc6e3
= <=>
2023-09-18 20:34:58 -0300

    comit inicial

---
+++ praticaJS1/js/exercicio2.js
@@ -0,0 +1,7 @@
+function tratadorDeCliqueExercicio2() {
+    data_atual = new Date(2023,09,04,8,40,28);
+    alert(data_atual);
+    // atual no seguinte formato:
+    // Horário: 8 PM : 40m : 28s
+    console.log('adicionar código na função tratadorDeCliqueExercicio2() em ./js/exercicio2.js')
+}
---
+++ praticaJS1/js/exercicio4.js
@@ -0,0 +1,10 @@
+function verificar(num) {
+    let numero = parseInt(prompt("Informe o Número"));
+    let intervalo = [30,50];
+    let intervalo2 = [60,100];
+
+    if (num>=intervalo[30]) and (num<=intervalo[50]) {
+       alert("VC Ganhou");
+    }
+
+}
