5424b86775ec0d8e6bc1d4dd9114a6b61c9607ea
= <=>
2023-09-18 21:20:01 -0300

    Update

--- praticaJS1/js/exercicio4.js
+++ praticaJS1/js/exercicio4.js
@@ -1,10 +1,16 @@
-function verificar(num) {
+function verificar() {
     let numero = parseInt(prompt("Informe o Número"));
     let intervalo = [30,50];
     let intervalo2 = [60,100];
 
-    if (num>=intervalo[30]) and (num<=intervalo[50]) {
-       alert("VC Ganhou");
+    if ((numero>=intervalo[0]) && (numero<=intervalo[1])) {
+       console.log(numero + "está no intervalo [30,50].");
+    } else if((numero>=intervalo2[0]) && (numero<=intervalo2[1])) {
+        console.log(numero + "está no intervalo [60,100].");
     }
+    else {
+        console.log("O número informado não está em nenhum dos dois intervalos.");
+    }
+    
 
 }
