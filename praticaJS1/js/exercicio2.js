0a242460ef34874b016d62b6d9ea588e3dc6adea
= <=>
2023-10-01 02:47:03 -0300

    update exercicio 2

--- praticaJS1/js/exercicio2.js
+++ praticaJS1/js/exercicio2.js
@@ -1,6 +1,9 @@
 function tratadorDeCliqueExercicio2() {
-    data_atual = new Date(2023,09,04,8,40,28);
-    alert(data_atual);
+    let dataAtual = new Date();
+    let horas = dataAtual.getHours();
+    let minutos = dataAtual.getMinutes();
+    let segundos = dataAtual.getSeconds();
+    alert("Hora atual: " + horas + ":" + minutos + ":" + segundos);
     // atual no seguinte formato:
     // Horário: 8 PM : 40m : 28s
     console.log('adicionar código na função tratadorDeCliqueExercicio2() em ./js/exercicio2.js')
